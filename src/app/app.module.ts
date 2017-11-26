96d8ebadb8ec2d9efcf2873e64075f07dd23a0a3
moralesVega <auxiliadoramorales@gmail.com>
2017-11-26 01:32:52 -0600

    merge resuelto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
-<<<<<<< HEAD
 
-=======
->>>>>>> 0c8f4de9d4f7003eef38a958cff7cd0efa3ed2fe
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {RegistroPage} from '../pages/registro/registro';
-<<<<<<< HEAD
 import {AvatarPage} from '../pages/avatar/avatar';
-=======
 import { DetalleRetoPage } from '../pages/detalle-reto/detalle-reto';
 
 
@@ -27,25 +22,19 @@ import { ProfileChallengesComponent } from '../components/profile-challenges/pro
 import { PublicityComponent } from '../components/publicity/publicity';
 import { PublicityChallengeComponent } from '../components/publicity-challenge/publicity-challenge';
 
->>>>>>> 0c8f4de9d4f7003eef38a958cff7cd0efa3ed2fe
 
 import {PerfilPage} from '../pages/perfil/perfil';
 import {RetosPage} from '../pages/retos/retos';
 import {TabPerfilRetosPage} from '../pages/tab-perfil-retos/tab-perfil-retos';
-<<<<<<< HEAD
 import { AvatarProvider } from '../providers/avatar/avatar';
-=======
 import { ChallengeProvider } from '../providers/challenge/challenge';
->>>>>>> 0c8f4de9d4f7003eef38a958cff7cd0efa3ed2fe
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     RegistroPage,
-<<<<<<< HEAD
-    AvatarPage
-=======
+    AvatarPage,
     PerfilPage,
     RetosPage,
     DetalleRetoPage,
@@ -59,7 +48,6 @@ import { ChallengeProvider } from '../providers/challenge/challenge';
     ProfileChallengesComponent,
     PublicityComponent,
     PublicityChallengeComponent
->>>>>>> 0c8f4de9d4f7003eef38a958cff7cd0efa3ed2fe
   ],
   imports: [
     BrowserModule,
@@ -73,9 +61,7 @@ import { ChallengeProvider } from '../providers/challenge/challenge';
     MyApp,
     HomePage,
     RegistroPage,
-<<<<<<< HEAD
-    AvatarPage
-=======
+    AvatarPage,
     PerfilPage,
     RetosPage,
     DetalleRetoPage,
@@ -88,17 +74,13 @@ import { ChallengeProvider } from '../providers/challenge/challenge';
     ProfileChallengesComponent,
     PublicityComponent,
     PublicityChallengeComponent
->>>>>>> 0c8f4de9d4f7003eef38a958cff7cd0efa3ed2fe
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-<<<<<<< HEAD
-    AvatarProvider
-=======
+    AvatarProvider,
     ChallengeProvider
->>>>>>> 0c8f4de9d4f7003eef38a958cff7cd0efa3ed2fe
   ]
 })
 export class AppModule {}
